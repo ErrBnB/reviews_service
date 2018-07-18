5514538324f1408ee80429d3f35ad457119bb428
Hack Reactor Student <student@hackreactor.com>
2018-07-18 12:50:20 -0700

    add backend for getOnePage

--- database/index.js
+++ database/index.js
@@ -5,14 +5,18 @@ const connection = mysql.createConnection(mysqlConfig);
 connection.connect();
 
 const getOnePage = function(params, callback) {
-  
-
+  connection.query('SELECT * FROM user_reviews WHERE (id <= 8)', params, function(error, results) {
+    if (error) {
+      console.log('DaTaBaSe connection error: ', error);
+    } else {
+      console.log('ReSuLtS from MySQL: ', results);
+      callback(results);
+    }
+  })
 }
 
 
-
-
-
+// getOnePage(1, () => {console.log('SoMeThInG')})
 
 module.exports = {
   getOnePage
--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../public'));
 
+app.get('/api/reviews/1', (req, res) => {
+  db.getOnePage(1, (results) => {
+    res.send(results)
+  })
+})
+
+
 
 app.listen(PORT, () => {
 console.log(`listening on port ${PORT}`);
